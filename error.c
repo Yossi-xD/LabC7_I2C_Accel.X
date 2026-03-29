a34ab80bd17251751f9ff03caee24050104ae25a
Yousef Yousef <Yousef.a.you2@gmail.com>
2026-03-29 15:29:32 +0300

    error

---
+++ error.c
@@ -0,0 +1,97 @@
+main.c: In function 'adc_init':
+main.c:304:1: error: expected declaration specifiers before 'ANSBbits'
+main.c:305:1: error: expected declaration specifiers before 'TRISBbits'
+main.c:307:1: error: expected declaration specifiers before 'AD1CON1bits'
+main.c:308:1: error: expected declaration specifiers before 'AD1CON1'
+main.c:309:1: error: expected declaration specifiers before 'AD1CON2'
+main.c:310:1: error: expected declaration specifiers before 'AD1CON3'
+main.c:311:1: error: expected declaration specifiers before 'AD1CHS'
+main.c:312:1: error: expected declaration specifiers before 'AD1CON1bits'
+main.c:313:1: error: expected declaration specifiers before 'DELAY_milliseconds'
+main.c:314:1: error: expected declaration specifiers before '}' token
+main.c:318:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:334:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:357:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:369:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:383:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:401:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:416:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:431:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:472:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:504:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:524:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:542:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:579:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:594:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:613:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:656:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:690:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:707:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:730:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:934:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:952:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:965:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:986:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:1058:1: error: expected '=', ',', ';', 'asm' or '__attribute__' before '{' token
+main.c:1268:1: error: expected '{' at end of input
+main.c: At top level:
+main.c:167:17: warning: 'g_sec' defined but not used
+main.c:168:17: warning: 'g_min' defined but not used
+main.c:169:17: warning: 'g_hour' defined but not used
+main.c:172:17: warning: 'g_day' defined but not used
+main.c:173:17: warning: 'g_month' defined but not used
+main.c:176:17: warning: 'g_al_hour' defined but not used
+main.c:177:17: warning: 'g_al_min' defined but not used
+main.c:178:17: warning: 'g_al_enabled' defined but not used
+main.c:179:17: warning: 'g_al_ringing' defined but not used
+main.c:180:17: warning: 'g_al_secs' defined but not used
+main.c:183:17: warning: 'g_disp' defined but not used
+main.c:184:17: warning: 'g_fmt' defined but not used
+main.c:187:17: warning: 'g_mode' defined but not used
+main.c:190:17: warning: 'g_menu_st' defined but not used
+main.c:191:17: warning: 'g_menu_cur' defined but not used
+main.c:192:17: warning: 'g_edit_val' defined but not used
+main.c:200:16: warning: 's_hand_sec' defined but not used
+main.c:201:16: warning: 's_hand_min' defined but not used
+main.c:202:16: warning: 's_hand_hour' defined but not used
+main.c:205:16: warning: 's_dig_ampm' defined but not used
+main.c:206:16: warning: 's_dig_day' defined but not used
+main.c:207:16: warning: 's_dig_month' defined but not used
+main.c:208:16: warning: 's_dig_alarm' defined but not used
+main.c:271:13: warning: 'hw_init' defined but not used
+main.c:283:13: warning: 'timer1_init' defined but not used
+main.c:295:13: warning: 'accel_init' defined but not used
+main.c:303:13: warning: 'adc_init' defined but not used
+main.c:222:17: warning: 'adc_read_pot' declared 'static' but never defined
+main.c:225:13: warning: 'rtc_advance' declared 'static' but never defined
+main.c:226:13: warning: 'alarm_check' declared 'static' but never defined
+main.c:227:13: warning: 'alarm_stop' declared 'static' but never defined
+main.c:230:16: warning: 'accel_read_xyz' declared 'static' but never defined
+main.c:231:16: warning: 'accel_shaken' declared 'static' but never defined
+main.c:232:16: warning: 'accel_flipped' declared 'static' but never defined
+main.c:235:13: warning: 'draw_line_any' declared 'static' but never defined
+main.c:236:13: warning: 'draw_hand' declared 'static' but never defined
+main.c:237:13: warning: 'draw_alarm_clock_icon' declared 'static' but never defined
+main.c:238:13: warning: 'draw_corner_clock' declared 'static' but never defined
+main.c:239:13: warning: 'draw_analog_ticks' declared 'static' but never defined
+main.c:242:13: warning: 'render_clock' declared 'static' but never defined
+main.c:243:13: warning: 'render_digital' declared 'static' but never defined
+main.c:244:13: warning: 'render_digital_update' declared 'static' but never defined
+main.c:245:13: warning: 'render_analog' declared 'static' but never defined
+main.c:246:13: warning: 'render_analog_update' declared 'static' but never defined
+main.c:247:13: warning: 'render_menu' declared 'static' but never defined
+main.c:248:13: warning: 'render_alarm' declared 'static' but never defined
+main.c:251:13: warning: 'menu_enter_state' declared 'static' but never defined
+main.c:252:13: warning: 'menu_exit' declared 'static' but never defined
+main.c:253:13: warning: 'menu_s1_press' declared 'static' but never defined
+main.c:254:13: warning: 'menu_s2_press' declared 'static' but never defined
+make[2]: *** [build/default/production/main.o] Error 255
+make[1]: *** [.build-conf] Error 2
+make: *** [.build-impl] Error 2
+nbproject/Makefile-default.mk:196: recipe for target 'build/default/production/main.o' failed
+make[2]: Leaving directory 'C:/Users/youse/Desktop/Software_Engineering/Microship/LabC7_I2C_Accel.X'
+nbproject/Makefile-default.mk:85: recipe for target '.build-conf' failed
+make[1]: Leaving directory 'C:/Users/youse/Desktop/Software_Engineering/Microship/LabC7_I2C_Accel.X'
+nbproject/Makefile-impl.mk:39: recipe for target '.build-impl' failed
+
+BUILD FAILED (exit value 2, total time: 995ms)
